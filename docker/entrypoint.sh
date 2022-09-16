8996025d412fd6060e5efc09096c301f6d124ad6
Gabriel <grossiwm@gmail.com>
2022-09-16 03:52:50 -0300

    change java agent location

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,5 @@ export RABBIT_PASSWORD=$RABBIT_PASSWORD
 export RABBIT_HOST=$RABBIT_HOST          
 export RABBIT_PORT=$RABBIT_PORT      
 
-java -javaagent:/var/dd-java-agent.jar -Ddd.service=news-payment-service \
+java -javaagent:/var/quarkus-app/dd-java-agent.jar -Ddd.service=news-payment-service \
 -Ddd.env=prod -Ddd.logs.injection=true -jar /var/quarkus-app/quarkus-run.jar
