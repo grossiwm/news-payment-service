432b563c0b4db1e3ba8542a80d1a48e6d7e9a934
Gabriel <grossiwm@gmail.com>
2022-09-16 01:57:18 -0300

    remove java agent

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,4 @@ export RABBIT_PASSWORD=$RABBIT_PASSWORD
 export RABBIT_HOST=$RABBIT_HOST          
 export RABBIT_PORT=$RABBIT_PORT      
 
-java -javaagent:/var/dd-java-agent.jar -Ddd.service=news-payment-service \
--Ddd.env=prod -Ddd.logs.injection=true -jar /var/quarkus-app/quarkus-run.jar
+java -jar /var/quarkus-app/quarkus-run.jar
